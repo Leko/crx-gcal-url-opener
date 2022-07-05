31d2ea1044de67637ff5ad19ee20c28134cef205
Leko <leko.noor@gmail.com>
2022-07-05 21:43:53 +0900

    chore: add link to privacy policy

--- src/popup.tsx
+++ src/popup.tsx
@@ -115,6 +115,18 @@ function App() {
           </Alert>
         </Box>
       )}
+      <Container component="footer">
+        <Typography variant="body2">
+          &copy; Leko{" | "}
+          <a
+            href="https://leko.jp/crx-gcal-url-opener/#%E3%83%97%E3%83%A9%E3%82%A4%E3%83%90%E3%82%B7%E3%83%BC%E3%81%B8%E3%81%AE%E5%8F%96%E3%82%8A%E7%B5%84%E3%81%BF"
+            target="_blank"
+            rel="noopener"
+          >
+            Privacy Policy
+          </a>
+        </Typography>
+      </Container>
     </div>
   );
 }
