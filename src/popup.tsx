9ec553aacc16e3942f31fb8dd4f4114c7bec1403
Leko <leko.noor@gmail.com>
2022-10-20 21:22:28 +0900

    refactor: enable noUnusedLocals and remove unused codes

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useMemo, useState } from "react";
+import React, { useLayoutEffect, useMemo, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Box } from "@mui/material";
 import { ErrorBoundary } from "./components/ErrorBoundary";
