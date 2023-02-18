548ff6b0126a0494a8cbe938c0df496b68c961fe
Leko <leko.noor@gmail.com>
2023-02-18 22:48:48 +0900

    chore: headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,12 +5,24 @@ const ciConfig: Partial<PlaywrightTestConfig> = {
   retries: 2,
   workers: 1,
   reporter: "github",
+  use: {
+    actionTimeout: 30000,
+    trace: "on-first-retry",
+    screenshot: "on",
+    headless: true,
+  },
 };
 const localConfig: Partial<PlaywrightTestConfig> = {
   forbidOnly: false,
   retries: 0,
   workers: undefined,
   reporter: "dot",
+  use: {
+    actionTimeout: 0,
+    trace: "on-first-retry",
+    screenshot: "on",
+    headless: false,
+  },
 };
 
 /**
@@ -29,17 +41,6 @@ export default defineConfig({
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-    screenshot: "on",
-  },
   /* Configure projects for major browsers */
   projects: [
     {
